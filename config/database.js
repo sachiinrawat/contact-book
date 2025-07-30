f3958728836b3921f41eab5ae8b3c7b2544c538a
sachiinrawat <oncetechiene@gmail.com>
2025-07-30 10:17:08 +0530

    ss

--- config/database.js
+++ config/database.js
@@ -5,6 +5,9 @@ dotenv.config();
 // Database Connection
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("Database Connected."));
 };
